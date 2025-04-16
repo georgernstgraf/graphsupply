bbc34a7855d3e8081fcd2b04185c35ef82008077
Georg Graf (SPG) <grafg@spengergasse.at>
2025-04-16 11:19:12 +0200

    weighted

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "@std/assert/equals";
+import { assert, assertEquals } from "@std/assert";
 import { Hono } from "jsr:@hono/hono";
 
 const base_url = Deno.env.get("BASE_URL");
@@ -15,7 +15,32 @@ if (isNaN(listenPort)) {
 }
 const base_url_parts = base_url.split("/");
 const prefix = base_url_parts.at(-1);
+const adjacency_simple = "adjacency-simple";
+const adjacency_weighted = "adjacency-weighted";
 
+function isQuadraticSymmetricNumberArray(matrix: number[][]): boolean {
+    // TODO make three distinct functions
+    const n = matrix.length;
+    for (let i = 0; i < n; i++) {
+        // Check if the row length is equal to n
+        if (matrix[i].length !== n) {
+            return false;
+        }
+        // and if the values are numbers
+        for (const value of matrix[i]) {
+            if (typeof value !== "number") {
+                return false;
+            }
+        }
+        // Check if the matrix is symmetric
+        for (let j = 0; j < i; j++) {
+            if (matrix[i][j] !== matrix[j][i]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
 const honoOptions = {
     port: listenPort, // Example: Use port 8080
     hostname,
@@ -24,7 +49,7 @@ const honoOptions = {
         console.log(
             `@ ${
                 new Date().toLocaleTimeString()
-            } graphsupply listening on http://${addr.hostname}:${addr.port}`,
+            } graphsupply listening on http://${addr.hostname}:${addr.port}/${prefix}`,
         );
     },
 };
@@ -91,13 +116,15 @@ app.get(`/${prefix}/andy-json/:filename`, async (c) => {
         );
     }
 });
-app.get(`/${prefix}/matrix-csv`, async (c) => {
+app.get(`/${prefix}/${adjacency_simple}`, async (c) => {
     const files = [];
     try {
-        for await (const dirEntry of Deno.readDir("./graphs/matrix-csv")) {
+        for await (
+            const dirEntry of Deno.readDir(`./graphs/${adjacency_simple}`)
+        ) {
             if (dirEntry.isFile) {
                 files.push(
-                    `${base_url}/matrix-csv/${dirEntry.name}`,
+                    `${base_url}/${adjacency_simple}/${dirEntry.name}`,
                 );
             }
         }
@@ -114,9 +141,9 @@ app.get(`/${prefix}/matrix-csv`, async (c) => {
         );
     }
 });
-app.get(`/${prefix}/matrix-csv/:filename`, async (c) => {
+app.get(`/${prefix}/${adjacency_simple}/:filename`, async (c) => {
     const filename = c.req.param("filename");
-    const filePath = `./graphs/matrix-csv/${filename}`;
+    const filePath = `./graphs/${adjacency_simple}/${filename}`;
     try {
         const fileContent = await Deno.readTextFile(filePath);
         const lines = fileContent.split("\n");
@@ -131,14 +158,63 @@ app.get(`/${prefix}/matrix-csv/:filename`, async (c) => {
                 return value;
             });
         });
-        const matrixLines = matrix.length;
-        const matrixColumns = matrix[0].length;
-        matrix.forEach((row) => {
-            assertEquals(matrixColumns, row.length);
-        });
+        assert(isQuadraticSymmetricNumberArray(matrix));
         return c.json({
-            "lines": matrixLines,
-            "columns": matrixColumns,
+            "lines": matrix.length,
+            "columns": matrix.length,
+            matrix,
+        });
+    } catch (error) {
+        console.error(`Error reading file ${filePath}:`, error);
+        const errorMessage = error instanceof Error
+            ? error.message
+            : String(error);
+        return c.json(
+            { error: `Failed to read file: ${errorMessage}` },
+            500,
+        );
+    }
+});
+app.get(`/${prefix}/${adjacency_weighted}`, async (c) => {
+    const files = [];
+    try {
+        for await (
+            const dirEntry of Deno.readDir(`./graphs/${adjacency_weighted}`)
+        ) {
+            if (dirEntry.isFile) {
+                files.push(
+                    `${base_url}/${adjacency_weighted}/${dirEntry.name}`,
+                );
+            }
+        }
+        files.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+        return c.json(files);
+    } catch (error) {
+        console.error("Error reading directories:", error);
+        const errorMessage = error instanceof Error
+            ? error.message
+            : String(error);
+        return c.json(
+            { error: `Failed to read file: ${errorMessage}` },
+            500,
+        );
+    }
+});
+app.get(`/${prefix}/${adjacency_weighted}/:filename`, async (c) => {
+    const filename = c.req.param("filename");
+    const filePath = `./graphs/${adjacency_weighted}/${filename}`;
+    try {
+        const fileContent = await Deno.readTextFile(filePath);
+        const json = JSON.parse(fileContent);
+        const matrix = json.matrix;
+        assert(
+            isQuadraticSymmetricNumberArray(matrix),
+            "isQuadraticSymmetricNumberArray() failed",
+        );
+        return c.json({
+            "lines": matrix.length,
+            "columns": matrix.length,
+            "nodes": json.nodes,
             matrix,
         });
     } catch (error) {
