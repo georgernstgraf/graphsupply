24b2f13670b4e1cbb5a102b1f236068367a964d2
Georg Graf (SPG) <grafg@spengergasse.at>
2025-05-01 23:14:02 +0200

    type

--- libbe/types.ts
+++ libbe/types.ts
@@ -1,1 +1,17 @@
-export type graphJson = { matrix: number[][] };
+//export type graphJson = { matrix: number[][] };
+export interface graphJson {
+    matrix: number[][];
+    "metadata": {
+        "params": {
+            "nodes": number;
+            "density": number;
+            "directed": boolean;
+            "weighted": boolean;
+            "loops": boolean;
+        };
+    };
+    "node_names"?: string[];
+    "edges": 8;
+    "message": "Graph generated with 80% density";
+    "understood_params": Record<string, string>;
+}
